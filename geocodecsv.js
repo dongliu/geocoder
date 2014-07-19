ea7ca2d8c85709de75d72fb1b92b54e277d5fb17
Dong Liu <edongliu@gmail.com>
2014-07-19 01:07:47 -0400

    csv file parse-geocode-write piping tested

--- geocodecsv.js
+++ geocodecsv.js
@@ -1,7 +1,16 @@
+/**
+ * @fileOverview read a csv file with addresses and output a csv file with corresponding rooftop geocodes if found.
+ * @author Dong Liu
+ */
+
 var csv = require('csv'),
   fs = require('fs'),
   path = require('path'),
-  inputPath, realPath;
+  geo = require('./lib/geo'),
+  inputPath,
+  realPath,
+  targetPath,
+  index = -1;
 
 if (process.argv.length === 2) {
   inputPath = './example/test.csv';
@@ -9,12 +18,38 @@ if (process.argv.length === 2) {
 
 if (process.argv.length > 2) {
   inputPath = process.argv[2];
-} 
-
-filePath = path.resolve(__dirname, inputPath);
-
-if (!fs.existsSync(filePath)) {
-  return console.warn(filePath + ' does not exist');
 }
 
-console.log('get rooftop geocodes of the addresses in ' + filePath);
+realPath = path.resolve(__dirname, inputPath);
+
+if (!fs.existsSync(realPath)) {
+  console.warn(realPath + ' does not exist');
+  process.exit(1);
+}
+
+targetPath = path.join(path.dirname(realPath), Date.now() + '.csv');
+
+console.log('read the addresses from ' + realPath + ', and output corresponding rooftop geocodes to ' + targetPath);
+
+fs.createReadStream(realPath)
+  .pipe(csv.parse())
+  .pipe(csv.transform(function (record, cb) {
+    setImmediate(function () {
+      index += 1;
+      if (index === 0) {
+        record.push('formatted', 'geocode');
+        cb(null, record);
+      } else {
+        geo.rooftopCode(record[0], function (err, data) {
+          if (data !== null) {
+            record.push(data.formatted_address, '(' + data.location.lat + ',' + data.location.lng + ')');
+          } else {
+            record.push('', '');
+          }
+          cb(null, record);
+        });
+      }
+    });
+  }))
+  .pipe(csv.stringify())
+  .pipe(fs.createWriteStream(targetPath));
